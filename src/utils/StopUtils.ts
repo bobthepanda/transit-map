7670d1e05b63f7c3231295a5310a3f39989b20ab
bobthepanda <henrybchin@gmail.com>
2022-12-28 14:47:25 -0800

    Add prettier-ignore to stops to hide

--- src/utils/StopUtils.ts
+++ src/utils/StopUtils.ts
@@ -1,28 +1,14 @@
+// prettier-ignore
 export const STOPS_TO_HIDE_TEXT: string[] = [
-  'C 11',
-  'C 09',
-  'C 08',
-  'H 08',
-  'H 09',
+  'C 11', 'C 09', 'C 08',
+  'H 08', 'H 09',
   'M 17',
-  'JC 01',
-  'JY 01',
-  'JK 26',
-  'JT 01', // Tokyo
-  'JY 30',
-  'Y 18', // Yurakucho
-  'JY 29',
-  'JK 24',
-  'JT 02',
-  'G 08',
-  'A 10', // Shimbashi,
-  'H 09',
-  'M 16', // Ginza
+  'JC 01', 'JY 01',  'JK 26', 'JT 01', // Tokyo
+  'JY 30', 'Y 18', // Yurakucho
+  'JY 29', 'JK 24', 'JT 02', 'G 08', 'A 10', // Shimbashi,
+  'H 09', 'M 16', // Ginza
   'H 10', // Higashi-Ginza
-  'T 09',
-  'T 10',
-  'T 11',
+  'T 09', 'T 10', 'T 11',
   'G 11',
-  'JY 02',
-  'JY 03',
-];
+  'JY 02', 'JY 03',
+]
