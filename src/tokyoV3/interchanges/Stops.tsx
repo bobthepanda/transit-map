9156258a386a61abc2e6a6509747d1a15d27884c
bobthepanda <henrybchin@gmail.com>
2024-12-08 14:01:49 -0800

    Remove existing stops

--- src/tokyoV3/interchanges/Stops.tsx
+++ src/tokyoV3/interchanges/Stops.tsx
@@ -1,74 +1,12 @@
-import InsideYamanote from './InsideYamanote/InsideYamanote';
-import { Asagaya } from './jr/Asagaya';
-import { DenEnChofu } from './jr/DenEnChofu';
-import { Hachioji } from './jr/Hachioji';
-import { HigashiNakano } from './jr/HigashiNakano';
-import { Hiyoshi } from './jr/Hiyoshi';
-import JR from './jr/JR';
-import { Jiyugaoka } from './jr/Jiyugaoka';
-import { Kichijoji } from './jr/Kichijoji';
-import { Kikuna } from './jr/Kikuna';
-import { Koenji } from './jr/Koenji';
-import { Mitaka } from './jr/Mitaka';
-import { FutakoTamagawa, Futakoshinchi, Takatsu } from './jr/Mizonokuchi';
-import { Motosumiyoshi } from './jr/Motosumiyoshi';
-import { MusashiSakai } from './jr/MusashiSakai';
-import { NakaMeguro } from './jr/NakaMeguro';
-import { Nakano } from './jr/Nakano';
-import { NakanoSakue } from './jr/NakanoSakue';
-import { NishiKokubunji } from './jr/NishiKokubunji';
-import { NishiOgikubo } from './jr/NishiOgikubo';
-import { Ogikubo } from './jr/Ogikubo';
-import { IkegamiLine, OimachiLine, Ookayama, TokyuKamataLines } from './jr/Ookayama';
-import { Sangenjaya } from './jr/Sangenjaya';
-import { ShinMaruko } from './jr/ShinMaruko';
-import { Tachikawa } from './jr/Tachikawa';
-import { Tamagawa } from './jr/Tamagawa';
-
-const WesternTokyo = () => {
-    return (
-        <>
-            <HigashiNakano />
-            <Nakano />
-            <Koenji />
-            <Asagaya />
-            <Ogikubo />
-            <NishiOgikubo />
-            <Kichijoji />
-            <Mitaka />
-            <Tachikawa />
-            <NishiKokubunji />
-            <NakanoSakue />
-            <MusashiSakai />
-            <ShinMaruko />
-            <Tamagawa />
-            <DenEnChofu />
-            <Jiyugaoka />
-            <Ookayama />
-            <NakaMeguro />
-            <Takatsu />
-            <Futakoshinchi />
-            <FutakoTamagawa />
-            <Sangenjaya />
-            <OimachiLine />
-            <IkegamiLine />
-            <TokyuKamataLines />
-            <Motosumiyoshi />
-            <Hiyoshi />
-            <Kikuna />
-            <Hachioji />
-        </>
-    );
-};
-
 const Interchanges = () => {
-    return (
-        <g id="interchanges">
-            <InsideYamanote />;
-            <JR />
-            <WesternTokyo />;
-        </g>
-    );
+    return null;
+    // return (
+    //     <g id="interchanges">
+    //         <InsideYamanote />;
+    //         <JR />
+    //         <WesternTokyo />;
+    //     </g>
+    // );
 };
 
 export default Interchanges;
--- src/tokyoV3/interchanges/jr/NAMBU_SLOPE.tsx
+++
@@ -1,5 +0,0 @@
-import { MAJOR_LINE } from '../../../map/GridLines';
-import { OFFSET } from '../../../utils/CommonCoordinates';
-import { S, scaleToUnitX } from '../../../utils/PathUtils';
-
-export const NAMBU_SLOPE = scaleToUnitX(S, MAJOR_LINE + OFFSET * 2);
