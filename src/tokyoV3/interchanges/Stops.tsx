fc8d03ec4a08641c6e8e9b3b889ceac22fc032ab
bobthepanda <henrybchin@gmail.com>
2024-12-15 18:19:26 -0800

    Memoize stop locations

--- src/tokyoV3/interchanges/Stops.tsx
+++ src/tokyoV3/interchanges/Stops.tsx
@@ -1,9 +1,12 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
 import { Stop } from '../../symbols/BasicStop';
 import { RootState } from '../../tokyo/redux/store';
 
+const stationCodesSelector = createSelector([(state: RootState) => state?.stopDefinition], (stopDefinition) => Object.keys(stopDefinition));
+
 const Interchanges = () => {
-    const stationCodes: string[] = useSelector((state: RootState) => Object.keys(state?.stopDefinition));
+    const stationCodes: string[] = useSelector(stationCodesSelector);
 
     return stationCodes.map((code) => {
         return <Stop key={code} stationCode={code} />;
