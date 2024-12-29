b9c6623ff5549afb0f1133d03e5f5d75bec12ce1
bobthepanda <henrybchin@gmail.com>
2024-12-28 20:58:29 -0800

    Move css files

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import './tokyo/colors/jr-east-colors.css';
 import { store } from './tokyo/redux/store';
 
 const router = createBrowserRouter([
